002060fc552894bffdbf0da1d604ee373740f44c
Heochan <heochan01@korea.ac.kr>
2025-01-03 21:53:34 +0900

    Fix: api route 변경 - '/api/v0/travel-time/(이동수단)'

--- sigo-travel-time/src/routes.rs
+++ sigo-travel-time/src/routes.rs
@@ -5,6 +5,7 @@ use super::handlers::*;
 use actix_web::web;
 
 pub fn travel_time_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/travel-time").route("/", web::post().to(get_travel_time_by_transit)));
+    let api_path = "/api/v0/travel-time";
+    cfg.service(web::scope(&api_path).route("/transit", web::post().to(get_travel_time_by_transit)));
     cfg.service(web::scope("/test").route("/", web::get().to(test_handler)));
 }
