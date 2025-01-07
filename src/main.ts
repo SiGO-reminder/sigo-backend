aeb8b69cbf2c5854c6e8d4a6f6f92a9e9ee5cc63
염정윤 <kori0423@naver.com>
2025-01-08 06:37:22 +0900

    modify api endpoint

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //전역 경로 프리픽스
+  app.setGlobalPrefix('api/v0');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
