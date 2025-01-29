9e736460c9e86e02735b5deae0c117d28ef676c3
Heochan <heochan01@korea.ac.kr>
2025-01-29 14:01:48 +0900

    Feat: tmap_transit_response_model 생성

--- sigo-travel-time/src/models/mod.rs
+++ sigo-travel-time/src/models/mod.rs
@@ -2,8 +2,10 @@
 
 mod api_main_model;
 mod tmap_driving_response_model;
+mod tmap_transit_response_model;
 mod tmap_walking_response_model;
 
 pub use api_main_model::{RequestBody, TMAPAPIInput, Transport};
 pub use tmap_driving_response_model::TmapDrivingResponse;
+pub use tmap_transit_response_model::TmapTransitResponse;
 pub use tmap_walking_response_model::TmapWalkingResponse;
---
+++ sigo-travel-time/src/models/tmap_transit_response_model.rs
@@ -0,0 +1,315 @@
+// {
+//   "metaData": {
+//     "plan": {
+//         "itineraries": [
+//             {
+//                 "fare": {
+//                     "regular": {
+//                         "currency": {
+//                             "currency": "원",
+//                             "currencyCode": "KRW",
+//                             "symbol": "￦"
+//                         },
+//                         "totalFare": 1500
+//                     }
+//                 },
+//                 "legs": [
+//                     {
+//                         "distance": 667,
+//                         "end": {
+//                             "lat": 37.57753888888889,
+//                             "lon": 127.03011111111113,
+//                             "name": "용두동사거리"
+//                         },
+//                         "mode": "WALK",
+//                         "sectionTime": 651,
+//                         "start": {
+//                             "lat": 37.58276551823443,
+//                             "lon": 127.02961696519652,
+//                             "name": "출발지"
+//                         },
+//                         "steps": [
+//                             {
+//                                 "description": "보행자도로 을 따라 7m 이동",
+//                                 "distance": 7,
+//                                 "linestring": "127.02974,37.58287 127.0298,37.582825",
+//                                 "streetName": "보행자도로"
+//                             },
+//                             {
+//                                 "description": "직진 후 39m 이동 ",
+//                                 "distance": 39,
+//                                 "linestring": "127.0298,37.582825 127.02985,37.582794 127.02975,37.582497",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "횡단보도 후 보행자도로 을 따라 23m 이동 ",
+//                                 "distance": 23,
+//                                 "linestring": "127.02975,37.582497 127.02988,37.58233",
+//                                 "streetName": "보행자도로"
+//                             },
+//                             {
+//                                 "description": "바이더웨이 안암로타리점 에서 직진 후 안암로 을 따라 11m 이동 ",
+//                                 "distance": 11,
+//                                 "linestring": "127.02988,37.58233 127.02982,37.582294 127.02978,37.582264",
+//                                 "streetName": "안암로"
+//                             },
+//                             {
+//                                 "description": "횡단보도 후 보행자도로 을 따라 18m 이동 ",
+//                                 "distance": 18,
+//                                 "linestring": "127.03039,37.57873 127.03042,37.578564",
+//                                 "streetName": "보행자도로"
+//                             },
+//                             {
+//                                 "description": "직진 후 무학로 을 따라 68m 이동 ",
+//                                 "distance": 68,
+//                                 "linestring": "127.03042,37.578564 127.03044,37.57847 127.03047,37.5783 127.03048,37.578228 127.030495,37.578144 127.0305,37.57812 127.03068,37.578045",
+//                                 "streetName": "무학로"
+//                             },
+//                             {
+//                                 "description": "큰빛프라자약국 에서 우측 횡단보도 후 11m 이동 ",
+//                                 "distance": 11,
+//                                 "linestring": "127.03068,37.578045 127.03062,37.57796",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "직진 후 11m 이동 ",
+//                                 "distance": 11,
+//                                 "linestring": "127.03062,37.57796 127.03057,37.577915 127.030525,37.577904",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "횡단보도 후 15m 이동 ",
+//                                 "distance": 15,
+//                                 "linestring": "127.030525,37.577904 127.030365,37.577858",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "직진 후 13m 이동 ",
+//                                 "distance": 13,
+//                                 "linestring": "127.030365,37.577858 127.03029,37.577847 127.03029,37.57779",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "횡단보도 후 23m 이동 ",
+//                                 "distance": 23,
+//                                 "linestring": "127.03029,37.57779 127.0303,37.577644 127.030304,37.577583",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "직진 후 8m 이동 ",
+//                                 "distance": 8,
+//                                 "linestring": "127.030304,37.577583 127.03032,37.577553 127.03029,37.577522",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "횡단보도 후 보행자도로 을 따라 10m 이동 ",
+//                                 "distance": 10,
+//                                 "linestring": "127.03029,37.577522 127.03023,37.577442",
+//                                 "streetName": "보행자도로"
+//                             },
+//                             {
+//                                 "description": "동대문여성인력개발센터 에서 우회전 후 왕산로 을 따라 13m 이동 ",
+//                                 "distance": 13,
+//                                 "linestring": "127.03023,37.577442 127.030106,37.5775",
+//                                 "streetName": "왕산로"
+//                             }
+//                         ]
+//                     },
+//                     {
+//                         "Lane": [
+//                             {
+//                                 "route": "간선:720",
+//                                 "routeColor": "0068B7",
+//                                 "routeId": "2870001",
+//                                 "service": 1,
+//                                 "type": 11
+//                             },
+//                             {
+//                                 "route": "간선:N72",
+//                                 "routeColor": "0068B7",
+//                                 "routeId": "1058981001",
+//                                 "service": 0,
+//                                 "type": 11
+//                             }
+//                         ],
+//                         "distance": 2245,
+//                         "end": {
+//                             "lat": 37.58458888888889,
+//                             "lon": 127.0526611111111,
+//                             "name": "떡전교앞"
+//                         },
+//                         "mode": "BUS",
+//                         "passShape": {
+//                             "linestring": "127.030089,37.577569 127.030400,37.577661 127.030581,37.577714 127.031100,37.577856 127.031703,37.577994 127.032378,37.578072 127.036372,37.578106 127.037419,37.578106 127.038231,37.578103 127.038414,37.578111 127.038728,37.578144 127.039303,37.578253 127.041822,37.579050 127.042417,37.579239 127.042503,37.579267 127.042603,37.579297 127.042672,37.579319 127.045083,37.580072 127.045256,37.580131 127.045558,37.580256 127.045828,37.580419 127.045908,37.580489 127.046206,37.580744 127.046331,37.580869 127.046442,37.580981 127.046731,37.581264 127.049350,37.583331 127.049964,37.583822 127.050247,37.584075 127.050572,37.584406 127.050800,37.584683 127.051364,37.585375 127.051472,37.585425 127.051581,37.585447 127.051803,37.585428 127.052169,37.585250 127.052278,37.585181 127.052361,37.585092 127.052689,37.584617"
+//                         },
+//                         "passStopList": {
+//                             "stationList": [
+//                                 {
+//                                     "index": 0,
+//                                     "lat": "37.577539",
+//                                     "lon": "127.030111",
+//                                     "stationID": "775322",
+//                                     "stationName": "용두동사거리"
+//                                 },
+//                                 {
+//                                     "index": 1,
+//                                     "lat": "37.578072",
+//                                     "lon": "127.037442",
+//                                     "stationID": "775336",
+//                                     "stationName": "제기동역.서울약령시"
+//                                 },
+//                                 {
+//                                     "index": 2,
+//                                     "lat": "37.579014",
+//                                     "lon": "127.041825",
+//                                     "stationID": "775365",
+//                                     "stationName": "청량리수산시장"
+//                                 },
+//                                 {
+//                                     "index": 3,
+//                                     "lat": "37.580481",
+//                                     "lon": "127.045953",
+//                                     "stationID": "775408",
+//                                     "stationName": "청량리역환승센터(3번승강장)"
+//                                 },
+//                                 {
+//                                     "index": 4,
+//                                     "lat": "37.584653",
+//                                     "lon": "127.050819",
+//                                     "stationID": "775521",
+//                                     "stationName": "서울성심병원"
+//                                 },
+//                                 {
+//                                     "index": 5,
+//                                     "lat": "37.584589",
+//                                     "lon": "127.052661",
+//                                     "stationID": "775518",
+//                                     "stationName": "떡전교앞"
+//                                 }
+//                             ]
+//                         },
+//                         "route": "간선:420",
+//                         "routeColor": "0068B7",
+//                         "routeId": "11455001",
+//                         "sectionTime": 674,
+//                         "service": 1,
+//                         "start": {
+//                             "lat": 37.57753888888889,
+//                             "lon": 127.03011111111113,
+//                             "name": "용두동사거리"
+//                         },
+//                         "type": 11
+//                     },
+//                     {
+//                         "distance": 240,
+//                         "end": {
+//                             "lat": 37.58388311414532,
+//                             "lon": 127.0540211996305,
+//                             "name": "도착지"
+//                         },
+//                         "mode": "WALK",
+//                         "sectionTime": 203,
+//                         "start": {
+//                             "lat": 37.58458888888889,
+//                             "lon": 127.0526611111111,
+//                             "name": "떡전교앞"
+//                         },
+//                         "steps": [
+//                             {
+//                                 "description": "전농로 을 따라 126m 이동",
+//                                 "distance": 126,
+//                                 "linestring": "127.05263,37.58457 127.052795,37.584347 127.05283,37.584286 127.05293,37.58414 127.052986,37.584053 127.05317,37.5838 127.05327,37.583656 127.053276,37.58365 127.05328,37.58364 127.05332,37.58358",
+//                                 "streetName": "전농로"
+//                             },
+//                             {
+//                                 "description": "주당클럽 에서 좌측 횡단보도 후 보행자도로 을 따라 25m 이동 ",
+//                                 "distance": 25,
+//                                 "linestring": "127.05332,37.58358 127.053566,37.583694",
+//                                 "streetName": "보행자도로"
+//                             },
+//                             {
+//                                 "description": "T world 시립대점 에서 직진 후 전농로 을 따라 6m 이동 ",
+//                                 "distance": 6,
+//                                 "linestring": "127.053566,37.583694 127.05359,37.58369 127.053635,37.58369",
+//                                 "streetName": "전농로"
+//                             },
+//                             {
+//                                 "description": "좌회전 후 9m 이동 ",
+//                                 "distance": 9,
+//                                 "linestring": "127.05422,37.583683 127.05422,37.583706 127.05423,37.583763",
+//                                 "streetName": ""
+//                             },
+//                             {
+//                                 "description": "좌회전 후 22m 이동 ",
+//                                 "distance": 22,
+//                                 "linestring": "127.05423,37.583763 127.05413,37.583775 127.054054,37.58379 127.053986,37.583805",
+//                                 "streetName": ""
+//                             }
+//                         ]
+//                     }
+//                 ],
+//                 "pathType": 2,
+//                 "totalDistance": 2973,
+//                 "totalTime": 1528,
+//                 "totalWalkDistance": 907,
+//                 "totalWalkTime": 854,
+//                 "transferCount": 0
+//             }
+//         ]
+//     },
+//     "requestParameters": {
+//       "airplaneCount": 0,
+//       "busCount": 1,
+//       "endX": "127.0540211996305",
+//       "endY": "37.58388311414532",
+//       "expressbusCount": 0,
+//       "ferryCount": 0,
+//       "locale": "ko",
+//       "reqDttm": "20250128212957",
+//       "startX": "127.02961696519651",
+//       "startY": "37.58276551823443",
+//       "subwayBusCount": 0,
+//       "subwayCount": 0,
+//       "trainCount": 0,
+//       "wideareaRouteCount": 0
+//     }
+//   }
+// }
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TmapTransitResponse {
+    pub meta_data: MetaData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MetaData {
+    pub plan: Plan,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Plan {
+    pub itineraries: Vec<Itinerary>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Itinerary {
+    #[serde(rename = "totalTime")]
+    pub total_time: u32,
+}
+
+impl TmapTransitResponse {
+    pub fn from_json(json_response: Value) -> Result<Self, serde_json::Error> {
+        let tmap_transit_response: TmapTransitResponse = serde_json::from_value(json_response)?;
+        Ok(tmap_transit_response)
+    }
+
+    pub fn get_total_time(&self) -> Option<i64> {
+        for itinerary in &self.meta_data.plan.itineraries {
+            return Some(itinerary.total_time as i64);
+        }
+        None
+    }
+}
